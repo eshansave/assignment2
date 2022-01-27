40e6a98139b3af33562ea51fee413ce004d645b1
eshansave <66193963+eshansave@users.noreply.github.com>
2022-01-27 14:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //const api_url = "<heroku_app_url>"
-const api_url = "https://gmr1289.herokuapp.com/user"
+const api_url = "https://esdassignement2.herokuapp.com/user"
 function loadData(records = []) {
     var table_data = "";
     for (let i = 0; i < records.length; i++) {
